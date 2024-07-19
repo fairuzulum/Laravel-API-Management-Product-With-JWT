2824cb55966f89e95c5f6479e204705b9ba1e018
Ahmad Fairuz Ulumuddin <usleunmdi32@gmail.com>
2024-07-19 09:36:57 +0700

    create model, migration, seeder

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->insert([
+            [
+                'product_category_id' => 1,
+                'name' => 'Smartphone',
+                'price' => 1000000,
+                'image' => 'smartphone.jpg',
+            ],
+            [
+                'product_category_id' => 2,
+                'name' => 'Sofa',
+                'price' => 400000,
+                'image' => 'sofa.jpg',
+            ],
+            [
+                'product_category_id' => 3,
+                'name' => 'Jeans',
+                'price' => 100000,
+                'image' => 'jeans.jpg',
+            ],
+        ]);
+    }
+}
